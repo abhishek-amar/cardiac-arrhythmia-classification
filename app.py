02d5524fdf6775c55e78c110561aa8d0be826232
Abhishek Amar <66067391+abhishek-amar@users.noreply.github.com>
2023-11-29 15:26:54 +0530

    Add cross validation with plots

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request
 from joblib import load
 import numpy as np
 import warnings
