e9a1a9e068b3272c41c641ddb3779e33b939836f
Abhishek Amar <66067391+abhishek-amar@users.noreply.github.com>
2023-12-11 18:00:57 +0530

    Add signup page

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 from joblib import load
 import numpy as np
 import warnings
@@ -23,17 +26,61 @@ class_names = ["Normal",
                "Left Ventricule hypertrophy",
                "Atrial Fibrillation or Flutter",
                "Others"]
+
+'''
+To create database, run the following if database does not exist:
+In the terminal:
+sqlite3 database.db
+CREATE TABLE User (
+username TEXT PRIMARY KEY,
+password TEXT NOT NULL
+);
+
+Or if the datavase already exists:
+In the terminal:
+sqlite3
+.open database.db
+CREATE TABLE User (
+username TEXT PRIMARY KEY,
+password TEXT NOT NULL
+);
+'''
+
 app = Flask(__name__)
+db_path = os.path.join(os.path.dirname(__file__), 'database.db')
+db_uri = f'sqlite:///{db_path}'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    username = db.Column(db.String(20), primary_key = True)
+    password = db.Column(db.String(20))
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-    error = " "
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        user = User.query.filter_by(username = request.form['username']).first()
+        if user is None:
+            return render_template('login.html', error = "Invalid Credentials. Please try again.")
         else:
-            return render_template('index.html', error = error)
-    return render_template('login.html', error = error)
+            if check_password_hash(user.password, request.form['password']):
+                return render_template('index.html')
+            else:
+                return render_template('login.html', error = "Invalid Credentials. Please try again.")
+    return render_template('login.html')
+
+@app.route('/signup', methods = ['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        user = User.query.filter_by(username = request.form['username']).first()
+        if user is not None:
+            return render_template('signup.html', message = "Username already exists.")
+        hashed_password = generate_password_hash(request.form['password'], method='sha256')
+        new_user = User(username = request.form['username'], password = hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template('signup.html', message = "Sign up successful.")
+    return render_template('signup.html')
 
 @app.route('/predict', methods = ['POST', 'GET'])
 def predict():
